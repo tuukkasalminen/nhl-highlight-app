d80f7513c810bead02d10ce98b814a4ec12efd94
Tuukka Salminen <tuukka.salminen@live.com>
2020-03-11 21:47:29 +0200

    Clean up + ESlint config

--- src/App.js
+++ src/App.js
@@ -1,79 +1,60 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import gameService from './services/games'
-import Game from './components/Game'
+import gameService from './services/games';
+import Game from './components/Game';
 
 class App extends Component {
-constructor(props) {
-  super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       games: [],
-      date: ''
-    }
+      date: '',
+    };
   }
-async componentDidMount () { 
-  //get yesterdays date to get the right content from api
-  const yesterday = new Date()
-  yesterday.setDate(yesterday.getUTCDate()-1)
-  const date = yesterday.getFullYear()+ '-' + (yesterday.getMonth()+1) + '-' + yesterday.getDate()
-  
-  //console.log(date)
 
-  //get the right dateform to be shown
-  const showDate = yesterday.getDate() + '/' + (yesterday.getMonth()+1) + '/' + yesterday.getFullYear()
-  this.setState({date: showDate})
+  async componentDidMount() {
+    // Get yesterdays date to get the right content from api
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getUTCDate() - 1);
+    const date = `${yesterday.getFullYear()}-${yesterday.getMonth() + 1}-${yesterday.getDate()}`;
 
-  //get schedule/games from api
-  const games = await gameService.getGames(date)
-  //only show games if all games are finished (and prevent error if there arent any games)
-/*   const finished = []
-  games.data.dates[0].games.forEach(game => {
-    if(game.status.abstractGameState === 'Final') {
-      finished.push(game.status.abstractGameState)
-    }
-  })
-  if (games.data.dates[0].games.length === finished.length) { */
-   this.setState({games: games.data.dates[0].games})
-  /* } */
-  
-  console.log(this.state.games, 'games')
-}
+    // Get the right date form to be shown
+    const showDate = `${yesterday.getDate()}/${yesterday.getMonth() + 1}/${yesterday.getFullYear()}`;
+    this.setState({ date: showDate });
 
-render() {
-  //link the right games' boxscore from nhl.com
-  const gameUrl = (id) => `https://www.nhl.com/gamecenter/${id}/recap/box-score`
+    // get schedule/games from api
+    const games = await gameService.getGames(date);
+    this.setState({ games: games.data.dates[0].games });
+  }
+
+  render() {
+    // Link the right games' boxscore from nhl.com
+    const gameUrl = (id) => `https://www.nhl.com/gamecenter/${id}/recap/box-score`;
+    const { games, date } = this.state;
 
     return (
       <div className="App">
-      <hr />
+        <hr />
         <header className="App-header">
-        <p>HOCKEY RECAPS</p>
-          <img src={require(`./logos/NHL.png`)} className="App-logo" alt="logo" />
+          <p>HOCKEY RECAPS</p>
+          <img src={require('./logos/NHL.png')} className="App-logo" alt="logo" />
         </header>
         <hr />
-{/*             <div className="info">
-              Click here for help
-            </div> */}
-          <div className="container">
-            <div className="row">
-            <div className="date">{this.state.date}</div>
+        <div className="container">
+          <div className="row">
+            <div className="date">{date}</div>
             <hr />
-              {this.state.games.map(game => 
-                  <Game 
-                    game={game}
-                    key={game.gamePk}
-                    expand={this.expand}
-                    gameUrl={gameUrl}
-                  />
-                  )}
-                  <hr />
-            </div>
-            <p className="copyright">NHL and the NHL Shield are registered trademarks of the National Hockey League. NHL and NHL team marks are the property of the NHL and its teams. © NHL 2018. All Rights Reserved.</p>
+            {games.map(game => (
+              <Game game={game} key={game.gamePk} expand={this.expand} gameUrl={gameUrl} />
+            ))}
+            <hr />
           </div>
+          <p className="copyright">NHL and the NHL Shield are registered trademarks of the National Hockey League. NHL and NHL team marks are the property of the NHL and its teams. © NHL 2018. All Rights Reserved.</p>
+        </div>
       </div>
     );
   }
 }
- 
+
 export default App;
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,95 +1,94 @@
-import React from 'react'
-import './Game.css'
+/* eslint-disable import/no-dynamic-require */
+import React from 'react';
+import './Game.css';
 
 class Game extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            expanded: false,
-            disabled: true,
-            selected: 'recap'/* ,
-            mobile: false,
-            mobileSupportOn: false */
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      expanded: false,
+      disabled: true,
+      selected: 'recap',
+    };
+  }
+
+  async componentDidMount() {
+    const { game } = this.props;
+    if (game.content.media.epg[3].items.length) {
+      this.setState({ disabled: false });
     }
+  }
 
-    async componentDidMount ()  {
-        if(this.props.game.content.media.epg[3].items.length)  {
-            this.setState({disabled: false})
-        }
-/*         if(window.innerWidth < 700) {
-            this.setState({mobile: true})
-        }  */
-    }
-     //toggle videoplayer on click
-    expand = () => {
-        this.setState({expanded: !this.state.expanded})
-        if(!this.state.expanded) this.setState({selected: 'recap'})
-    }
+  expand = () => {
+    const { expanded } = this.state;
+    this.setState({ expanded: !expanded });
+    if (!expanded) this.setState({ selected: 'recap' });
+  }
 
-    render(){
-        if(!this.state.disabled && this.state.expanded) {
-            return(
-                <div className="videoContainer">
-                    {this.state.mobile && 
-                    <p className="info">Video may sometimes be slow if you are on mobile. If you are experiencing this, please click the "Mobile Support"-button</p>}
-                    <div className="topContainer">
-                        <div className={"option" + (this.state.selected === 'recap' ? ' selected' :'')}
-                        onClick={e => this.setState({selected: 'recap'})}>
-                            Game Recap
-                        </div>
-                        <div className={"option" + (this.state.selected === 'extended' ? ' selected' :'')}
-                            onClick={e => this.setState({selected: 'extended'})}>
-                                Extended HL
-                            </div>
-                        <a className="option boxScore" target="_blank" rel="noopener noreferrer" href={this.props.gameUrl(this.props.game.gamePk)}>Box Score</a>
-                        {this.state.mobile && 
-                        <div className={"option" + (this.state.mobileSupportOn ? ' selected' :'')}
-                            onClick={e=> this.setState({mobileSupportOn: !this.state.mobileSupportOn})}>
-                            Mobile Support
-                        </div>}
-                        <div className="close" onClick={this.expand}>X</div>
-                    </div>
-                    <div className="embed-responsive embed-responsive-16by9">  
+  render() {
+    const {
+      disabled, expanded, selected,
+    } = this.state;
 
-                         {this.state.selected === 'recap' &&
-                         /*(this.state.mobileSupportOn ?
-                            <video className="embed-responsive-item" controls>
-                                <source type="video/mp4" src={this.props.game.content.media.epg[3].items[0].playbacks[2].url} />
-                            </video>
-                        :   */
-                            <video className="embed-responsive-item" controls>
-                                <source type="video/mp4" src={this.props.game.content.media.epg[3].items[0].playbacks[3].url} />
-                         </video>}
-
-                        {this.state.selected === 'extended' &&
-                        <video className="embed-responsive-item" controls>
-                            <source type="video/mp4" src={this.props.game.content.media.epg[2].items[0].playbacks[3].url} />
-                        </video>} 
-                    </div>
-                </div>
-            )
-        }
-/* 
-        window.innerWidth < 700 ? 
-        this.props.game.content.media.epg[2].items[0].playbacks[8].url.slice(5) :
-        this.props.game.content.media.epg[2].items[0].playbacks[9].url.slice(5)} 
-        allowFullScreen > */
-
-        return (
-            <div className={"col-lg-2 col-md-3 row-sm-12 game" + (this.state.disabled ? ' disabled' : '')} 
-                onClick={this.expand} key={this.props.game.gamePk}>
-            <div className="team">
-                <img className="teamLogo" src={require(`../logos/${this.props.game.teams.away.team.abbreviation}.svg`)} alt="" />
-                <p>{this.props.game.teams.away.team.abbreviation}</p>
+    const { game, gameUrl } = this.props;
+    if (!disabled && expanded) {
+      return (
+        <div className="videoContainer">
+          <div className="topContainer">
+            <div
+              className={`option + ${selected === 'recap' ? ' selected' : ''}`}
+              onClick={() => this.setState({ selected: 'recap' })}
+            >
+              { 'Game Recap' }
             </div>
-            <div className="team">
-                <img className="teamLogo" src={require(`../logos/${this.props.game.teams.home.team.abbreviation}.svg`)} alt="" />
-                <p>{this.props.game.teams.home.team.abbreviation}</p>
+            <div
+              className={`option${selected === 'extended' ? ' selected' : ''}`}
+              onClick={() => this.setState({ selected: 'extended' })}
+            >
+              { 'Extended HL' }
             </div>
-            </div>
-        )
+            <a className="option boxScore" target="_blank" rel="noopener noreferrer" href={gameUrl(game.gamePk)}>Box Score</a>
+            <div className="close" onClick={this.expand}>X</div>
+          </div>
+          <div className="embed-responsive embed-responsive-16by9">
+            {selected === 'recap'
+              && (
+              <video className="embed-responsive-item" controls>
+                <track default kind="captions" />
+                <source type="video/mp4" src={game.content.media.epg[3].items[0].playbacks[3].url} />
+              </video>
+              )
+            }
+
+            {selected === 'extended'
+            && (
+            <video className="embed-responsive-item" controls>
+              <track default kind="captions" />
+              <source type="video/mp4" src={game.content.media.epg[2].items[0].playbacks[3].url} />
+            </video>
+            )}
+          </div>
+        </div>
+      );
     }
+
+    return (
+      <div
+        className={`col-lg-2 col-md-3 row-sm-12 game${(disabled ? ' disabled' : '')}`}
+        onClick={this.expand}
+        key={game.gamePk}
+      >
+        <div className="team">
+          <img className="teamLogo" src={require(`../logos/${game.teams.away.team.abbreviation}.svg`)} alt="" />
+          <p>{game.teams.away.team.abbreviation}</p>
+        </div>
+        <div className="team">
+          <img className="teamLogo" src={require(`../logos/${game.teams.home.team.abbreviation}.svg`)} alt="" />
+          <p>{game.teams.home.team.abbreviation}</p>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Game
+export default Game;
